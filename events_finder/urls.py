ea9a61f0c30166a2d94ecb9fc0590a1c85cf4aa5
Agata Orzechowska <agata9orzechowska@gmail.com>
2019-12-02 21:19:01 +0100

    Reformat with Black

--- events_finder/urls.py
+++ events_finder/urls.py
@@ -15,28 +15,78 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, re_path
-from concerts.views import LandingPage, MainPage, ChooseFinder, BandConcertsFinder, ArtistConcertsFinder, \
-    DateConcertsFinder, CityConcertsFinder, ConcertDetails, TopConcerts, LikeRealConcert, ListOfDreamConcerts, \
-    SearchDreamConcert, LikeDreamConcert, AddDreamConcert, LoginView, LogoutView, RegisterUserView
+from concerts.views import (
+    LandingPage,
+    MainPage,
+    ChooseFinder,
+    BandConcertsFinder,
+    ArtistConcertsFinder,
+    DateConcertsFinder,
+    CityConcertsFinder,
+    ConcertDetails,
+    TopConcerts,
+    LikeRealConcert,
+    ListOfDreamConcerts,
+    SearchDreamConcert,
+    LikeDreamConcert,
+    AddDreamConcert,
+    LoginView,
+    LogoutView,
+    RegisterUserView,
+)
 
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', LandingPage.as_view(), name='loading-page'),
-    path('main/', MainPage.as_view(), name='main-page'),
-    path('choose_finder/', ChooseFinder.as_view(), name='choose-finder'),
-    path('band_concerts_finder/', BandConcertsFinder.as_view(), name='band-concerts-finder'),
-    path('artist_concerts_finder/', ArtistConcertsFinder.as_view(), name='artist-concerts-finder'),
-    path('date_concerts_finder/', DateConcertsFinder.as_view(), name='date-concerts-finder'),
-    path('city_concerts_finder/', CityConcertsFinder.as_view(), name='city-concerts-finder'),
-    re_path(r'^concert_details/(?P<id>(\d)+)', ConcertDetails.as_view(), name='concert-details'),
-    path('top10/', TopConcerts.as_view(), name='top-concerts'),
-    re_path(r'^like_realconcert/(?P<id>(\d)+)', LikeRealConcert.as_view(), name='realconcert-likes'),
-    path('list_of_dreamconcerts/', ListOfDreamConcerts.as_view(), name='list-of-dreamconcerts'),
-    path('search_dreamconcert/', SearchDreamConcert.as_view(), name='search-dreamconcert'),
-    re_path(r'^like_dreamconcert/(?P<id>(\d)+)', LikeDreamConcert.as_view(), name='dreamconcert-likes'),
-    path('add_dreamconcert/', AddDreamConcert.as_view(), name='add-dreamconcert'),
-    path('login/', LoginView.as_view(), name='login'),
-    path('logout/', LogoutView.as_view(), name='logout'),
-    path('register/', RegisterUserView.as_view(), name='register_user')
+    path("admin/", admin.site.urls),
+    path("", LandingPage.as_view(), name="loading-page"),
+    path("main/", MainPage.as_view(), name="main-page"),
+    path("choose_finder/", ChooseFinder.as_view(), name="choose-finder"),
+    path(
+        "band_concerts_finder/",
+        BandConcertsFinder.as_view(),
+        name="band-concerts-finder",
+    ),
+    path(
+        "artist_concerts_finder/",
+        ArtistConcertsFinder.as_view(),
+        name="artist-concerts-finder",
+    ),
+    path(
+        "date_concerts_finder/",
+        DateConcertsFinder.as_view(),
+        name="date-concerts-finder",
+    ),
+    path(
+        "city_concerts_finder/",
+        CityConcertsFinder.as_view(),
+        name="city-concerts-finder",
+    ),
+    re_path(
+        r"^concert_details/(?P<id>(\d)+)",
+        ConcertDetails.as_view(),
+        name="concert-details",
+    ),
+    path("top10/", TopConcerts.as_view(), name="top-concerts"),
+    re_path(
+        r"^like_realconcert/(?P<id>(\d)+)",
+        LikeRealConcert.as_view(),
+        name="realconcert-likes",
+    ),
+    path(
+        "list_of_dreamconcerts/",
+        ListOfDreamConcerts.as_view(),
+        name="list-of-dreamconcerts",
+    ),
+    path(
+        "search_dreamconcert/", SearchDreamConcert.as_view(), name="search-dreamconcert"
+    ),
+    re_path(
+        r"^like_dreamconcert/(?P<id>(\d)+)",
+        LikeDreamConcert.as_view(),
+        name="dreamconcert-likes",
+    ),
+    path("add_dreamconcert/", AddDreamConcert.as_view(), name="add-dreamconcert"),
+    path("login/", LoginView.as_view(), name="login"),
+    path("logout/", LogoutView.as_view(), name="logout"),
+    path("register/", RegisterUserView.as_view(), name="register_user"),
 ]
